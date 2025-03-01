0e50efd72055549e41788903ba8a1240e2f96916
Bede Abbe <bede.abbe@gmail.com>
2025-03-01 17:10:57 +0000

    changes

--- controllers/functions/email_helper.go
+++ controllers/functions/email_helper.go
@@ -12,15 +12,13 @@ func SendEmail(email string, link string) {
 
 	// Here we do it all: connect to our server, set up a message and send it
 
+	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
+	subject := "Subject: Test email from Go!\n"
+	body := "<html><body><table></table><tr><th>AMC Sales & Rentals</th></tr><tr></tr><tr><td>User invitation</td></tr><tr><td>" + link + "</td></tr><tr></tr><tr><a href='" + link + "'>Accept Invite</a></tr></body></html>"
+
 	to := []string{email}
 
-	msg := []byte("To: " + email + "\r\n" +
-
-		"Subject: Please click on the link below.\r\n" +
-
-		"\r\n" +
-
-		"<div style='color: blue'>" + link + ".\r\nThis link will expire in 4 hours.\r\n</div>")
+	msg := []byte(subject + mime + body)
 
 	err := smtp.SendMail("smtp.gmail.com:587", auth, "bede.abbe@gmail.com", to, msg)
 
