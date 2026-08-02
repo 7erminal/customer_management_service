05ca1ff93ea6f4131caa89d90705dd880fec6877
Terminal-Adawe <bede.abbe91@gmail.com>
2026-08-02 09:41:48 +0000

    customer

--- controllers/users.go
+++ controllers/users.go
@@ -386,7 +386,7 @@ func (c *UsersController) SignUp() {
 	} else {
 		// c.Data["json"] = err.Error()
 		logs.Error("User with email already exists...")
-		logs.Error("Error is ", err.Error())
+		// logs.Error("Error is ", err.Error())
 		var resp = responses.UserResponseDTO{StatusCode: 604, User: nil, StatusDesc: "User already exists. Username, email or mobile number already exists."}
 		c.Data["json"] = resp
 	}
